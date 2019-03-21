cfdb3235644aee7d679600b067bef4627919c2cb
mkaca <mskaca@edu.uwaterloo.ca>
2019-03-20 23:28:14 -0400

    Completed encoder functions

--- EncoderTest/EncoderTest/EncoderTest.ino
+++ EncoderTest/EncoderTest/EncoderTest.ino
@@ -1,12 +1,13 @@
-//#include <EncoderKanary.h>
 #include <TimeLib.h>
 
-#define intrL 10
+#define intrL 8
 #define intrR 11
 
 // for interrupt encoder
 // Wheel is going STRAIGHT if coutner is positive
 volatile unsigned long rRotsLocal, lRotsLocal;
+unsigned long lastIntrL;
+unsigned long lastIntrR;
 
 // init static variables
 int currentPosX = 0;
@@ -15,6 +16,9 @@ int currentPosYaw = 0;
 
 bool directionCWR = 1;
 bool directionCWL = 0;
+
+// UI sent command
+int acCode;
 
 // init encoder to go forward FIRST: left wheel = 0, right wheel = 1 with 8 magnets
 float wheelBase = 25.5 * 2.54;
@@ -26,20 +30,30 @@ int numOfMagnets = 8;
  * motor driver to be updated so that it returns CWL and CWR directions 
  */
 
-
-
 // updates leftCounter
 void updateLeft(){
-  if (directionCWL == 0)
+  if (millis() - lastIntrL > 80){ // if more than 80 ms passed since last encoder rotation
+    if (directionCWL == 0)
     {lRotsLocal++;}
-  else {lRotsLocal--;}
+    else {lRotsLocal--;}
+    
+    Serial.print("Millis: ");
+    Serial.println(millis());
+    lastIntrL = millis();
+  }
 }
 
 // updates rightCounter
 void updateRight(){
-  if (directionCWR == 1)
-    {lRotsLocal++;}
-  else {lRotsLocal--;}
+  if (millis() - lastIntrR > 80){ // if more than 80 ms passed since last encoder rotation
+    if (directionCWR == 1)
+    {rRotsLocal++;}
+    else {rRotsLocal--;}
+    
+    Serial.print("Millis: ");
+    Serial.println(millis());
+    lastIntrR = millis();
+  }
 }
 
 //resets counter
@@ -80,10 +94,28 @@ void updateCurrentPosition(int &curPosX, int &curPosY, int &curPosYaw)
   reset();
 }
 
+/* 
+ *  Updates required direction of motor
+ */
+void updateMotorDirections(){
+  if (acCode == 1 || acCode == 3 || acCode == 4 || acCode == 6){
+    directionCWR = 1;
+  }
+  else{
+    directionCWR = 0;
+  }
+  if (acCode == 2 || acCode == 6){
+    directionCWL = 1;
+  }
+  else{
+    directionCWL = 0;
+  }
+}
+
 void setup() {
   reset(); // sets encoder counts to 0
   Serial.begin(9600);
-  pinMode(intrL, INPUT_PULLUP);
+  pinMode(intrL, INPUT);
   pinMode(intrR, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(intrR), updateRight, RISING);
   attachInterrupt(digitalPinToInterrupt(intrL), updateLeft, RISING);
@@ -100,8 +132,10 @@ void loop() {
   Serial.println(currentPosY);
   Serial.println(currentPosYaw);
   Serial.println(" ");
-  updateCurrentPosition(currentPosX,currentPosY,currentPosYaw);
+  Serial.print( "lRotsLcal: ");
+  Serial.println(lRotsLocal);
+  //updateCurrentPosition(currentPosX,currentPosY,currentPosYaw);
 
-delay(200);
+delay(800);
 
 }
